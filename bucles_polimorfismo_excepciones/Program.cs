9fb1a8b7e6049859f3ff10d70e020ee73e40e401
Jose David <josee98mc@gmail.com>
2026-03-24 16:41:22 -0500

    Ejercicio de Polimorfismo

---
+++ bucles_polimorfismo_excepciones/Program.cs
@@ -0,0 +1,25 @@
+﻿
+var circ=new Circulo();
+circ.Radio=20;
+var result_area=circ.Area();
+var result_perimetro=circ.Perimetro();
+
+Console.WriteLine($"El area es: {result_area} y el perimetro es: {result_perimetro}");
+
+abstract class Figura
+{
+    public abstract double Area();
+
+    public abstract double Perimetro();
+}
+
+class Circulo : Figura
+{
+    public double Radio {get; set;}
+
+    public override double Area() =>Math.PI*Radio*Radio;
+    public override double Perimetro() => 2*Math.PI*Radio;
+        
+}
+
+
